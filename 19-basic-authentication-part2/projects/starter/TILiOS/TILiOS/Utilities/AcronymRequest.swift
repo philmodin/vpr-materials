18c51c7bbdf3e7a07b048819a030d018a69e3c75
Philip Modin <18331871+philmodin@users.noreply.github.com>
2022-11-11 19:18:59 -0600

    Chapter 19 complete

--- 19-basic-authentication-part2/projects/starter/TILiOS/TILiOS/Utilities/AcronymRequest.swift
+++ 19-basic-authentication-part2/projects/starter/TILiOS/TILiOS/Utilities/AcronymRequest.swift
@@ -1,30 +1,3 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
 
 import Foundation
 
@@ -83,19 +56,27 @@ struct AcronymRequest {
     completion: @escaping (Result<Acronym, ResourceRequestError>) -> Void
   ) {
     do {
+			guard let token = Auth().token else {
+				Auth().logout()
+				return
+			}
       var urlRequest = URLRequest(url: resource)
       urlRequest.httpMethod = "PUT"
       urlRequest.httpBody = try JSONEncoder().encode(updateData)
       urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+			urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
       let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, _ in
-        guard
-          let httpResponse = response as? HTTPURLResponse,
-          httpResponse.statusCode == 200,
-          let jsonData = data
-        else {
+        guard let httpResponse = response as? HTTPURLResponse else {
           completion(.failure(.noData))
           return
         }
+				guard httpResponse.statusCode == 200, let jsonData = data else {
+					if httpResponse.statusCode == 401 {
+						Auth().logout()
+					}
+					completion(.failure(.noData))
+					return
+				}
         do {
           let acronym = try JSONDecoder().decode(Acronym.self, from: jsonData)
           completion(.success(acronym))
@@ -110,8 +91,13 @@ struct AcronymRequest {
   }
 
   func delete() {
+		guard let token = Auth().token else {
+			Auth().logout()
+			return
+		}
     var urlRequest = URLRequest(url: resource)
     urlRequest.httpMethod = "DELETE"
+		urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
     let dataTask = URLSession.shared.dataTask(with: urlRequest)
     dataTask.resume()
   }
@@ -124,20 +110,27 @@ struct AcronymRequest {
       completion(.failure(.noID))
       return
     }
+		guard let token = Auth().token else {
+			Auth().logout()
+			return
+		}
     let url = resource
       .appendingPathComponent("categories")
       .appendingPathComponent("\(categoryID)")
     var urlRequest = URLRequest(url: url)
     urlRequest.httpMethod = "POST"
+		urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
     let dataTask = URLSession.shared
       .dataTask(with: urlRequest) { _, response, _ in
-        guard
-          let httpResponse = response as? HTTPURLResponse,
-          httpResponse.statusCode == 201
-        else {
+        guard let httpResponse = response as? HTTPURLResponse else {
           completion(.failure(.invalidResponse))
           return
         }
+				guard httpResponse.statusCode == 201 else {
+					if httpResponse.statusCode == 401 { Auth().logout() }
+					completion(.failure(.invalidResponse))
+					return
+				}
         completion(.success(()))
       }
     dataTask.resume()
--- 19-basic-authentication-part2/projects/starter/TILiOS/TILiOS/Utilities/ResourceRequest.swift
+++ 19-basic-authentication-part2/projects/starter/TILiOS/TILiOS/Utilities/ResourceRequest.swift
@@ -1,30 +1,3 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
 
 import Foundation
 
@@ -61,20 +34,27 @@ struct ResourceRequest<ResourceType> where ResourceType: Codable {
     completion: @escaping (Result<ResourceType, ResourceRequestError>) -> Void
   ) where CreateType: Codable {
     do {
+			guard let token = Auth().token else {
+				Auth().logout()
+				return
+			}
       var urlRequest = URLRequest(url: resourceURL)
       urlRequest.httpMethod = "POST"
       urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+			urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
       urlRequest.httpBody = try JSONEncoder().encode(saveData)
       let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, _ in
-        guard
-          let httpResponse = response as? HTTPURLResponse,
-          httpResponse.statusCode == 200,
-          let jsonData = data
-          else {
+        guard let httpResponse = response as? HTTPURLResponse else {
             completion(.failure(.noData))
             return
         }
-
+				guard httpResponse.statusCode == 200, let jsonData = data else {
+					if httpResponse.statusCode == 401 {
+						Auth().logout()
+					}
+					completion(.failure(.noData))
+					return
+				}
         do {
           let resource = try JSONDecoder().decode(ResourceType.self, from: jsonData)
           completion(.success(resource))
