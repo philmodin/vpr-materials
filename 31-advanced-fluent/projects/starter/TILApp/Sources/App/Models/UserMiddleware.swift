84f75526a18a48d5d2ea80274facbd8687968620
Philip Modin <18331871+philmodin@users.noreply.github.com>
2022-12-01 19:19:45 -0600

    Chapter 31 Lifecycle Hooks complete

---
+++ 31-advanced-fluent/projects/starter/TILApp/Sources/App/Models/UserMiddleware.swift
@@ -0,0 +1,21 @@
+
+import Fluent
+import Vapor
+
+struct UserMiddleware: ModelMiddleware {
+	func create(model: User, on db: Database, next: AnyModelResponder) -> EventLoopFuture<Void> {
+		User.query(on: db)
+			.filter(\.$username == model.username)
+			.count()
+			.flatMap { count in
+				guard count == 0 else {
+					let error = Abort(.badRequest, reason: "Username already exists")
+					return db.eventLoop.future(error: error)
+				}
+				return next.create(model, on: db).map {
+					let errorMessage: Logger.Message = "Created user with username \(model.username)"
+					db.logger.debug(errorMessage)
+				}
+			}
+	}
+}
